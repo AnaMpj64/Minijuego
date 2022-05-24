af76e2b09ed0cc94dce6bfed1ca4c634c151d308
AnaMpj64 <ana_maria_ggz@hotmail.com>
2022-05-24 12:24:24 -0500

    Se corrige ImputMismatch Exeption y aniade metodo

--- src/Instrucciones/Juego.java
+++ src/Instrucciones/Juego.java
@@ -1,15 +1,15 @@
 
 package Instrucciones;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Juego {
+    static Scanner sc = new Scanner(System.in);
+    static int numeroIngresado;
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
         System.out.println("Juego adivina un numero del 1 al 10");
-        System.out.println("Ingresa un numero:");
-        int numeroIngresado = sc.nextInt();
-        
+        getNumberByKeyword();
         int aleatorio;
         aleatorio = (int) (Math.random()*10 + 1);
         int distancia = aleatorio-numeroIngresado;
@@ -28,10 +28,24 @@ public class Juego {
         else if (distancia > 4 ||distancia < -4){
             System.out.println("Frio");
         }
-        numeroIngresado = sc.nextInt();
+        getNumberByKeyword();
         distancia = aleatorio-numeroIngresado;
       }
         System.out.println("El numero era:" +aleatorio);    
-        
     }
+    public static void getNumberByKeyword(){
+           while(true){
+           try{
+                System.out.println("Ingresa un numero:");
+                numeroIngresado = sc.nextInt();
+                if(numeroIngresado>=1&&numeroIngresado<=10){
+                    break;
+                }
+                System.out.println("El numero debe ser mayor a 0 y menor a 10");
+             } catch(InputMismatchException exeption){
+                 System.out.println("Numero invalido, por favor ingrese caracteres validos");
+                 sc.nextLine();
+             }   
+         }
+       }
 }
